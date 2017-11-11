a17f9686c25e3c3649064a115fd0d015d1d2148e
Chris Lane <chris@chris-allen-lane.com>
2017-11-11 11:39:59 -0500

    Audio scaling
    - Audio and video frequencies now scale in a linear (rather than
      mutiplicative) way
    - Minor refactoring
    - Changed some config values

--- js/app.js
+++ js/app.js
@@ -4,32 +4,28 @@ var params = {
   // strobe frequency
   frequency : 8,
 
-  // ratio of visual frequency to audio hertz
-  // TODO: this might be better as a log or something
-  ratio     : 150,
-
   // time spent at each frequency
-  interval  : 3 * 60 * 1000, // 3 minutes
+  interval  : 1.5 * 60 * 1000, // 1.5 minutes
 
   // frequency to step down per step (in hz)
   step      : 0.5,
+
+  // strobe parameters
+  strobe: {
+    target : document.getElementsByTagName('main')[0],
+    color  : '#00F',
+  },
+
+  // audio parameters
+  tone: {
+    type      : 'sine',
+    frequency : 150,
+  },
 };
 
 // log the runtime
 var time = (params.frequency / params.step) * params.interval / 60 / 1000;
 console.log('runtime: ' + time + ' minutes');
-
-// strobe parameters
-params.strobe = {
-  target : document.getElementsByTagName('main')[0],
-  color  : '#00F',
-};
-
-// audio parameters
-params.tone = {
-  type      : 'sine',
-  frequency : params.frequency * params.ratio, // base tone in hz
-};
 
 // begin
 strobe(params);
@@ -47,7 +43,7 @@ var step = function () {
 
   // step down the "master" frequency
   params.frequency      -= params.step;
-  params.tone.frequency  = params.ratio * params.frequency;
+  params.tone.frequency -= params.frequency;
 
   // and derive new specific frequencies
   strobe(params);
